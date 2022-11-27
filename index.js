a689e1dacf5baa999690ab38750c6aa19a50dc94
trofim <58886419+trofimpanasik@users.noreply.github.com>
2022-11-27 15:41:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,33 +15,12 @@ const server = httpServer.createServer(app)
 const PORT = process.env.PortServer
 
 //const whitelist = ["http://localhost:5173", "https://shop-ba9bb.web.app"]
-// const corsOptions = {
-//   origin: '*',
-//   optionsSuccessStatus: 200
-// }
-// app.use(cors(corsOptions))
-const allowCors = fn => async (req, res) => {
-  res.setHeader('Access-Control-Allow-Credentials', true)
-  res.setHeader('Access-Control-Allow-Origin', '*')
-  // another common pattern
-  // res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
-  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT')
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version'
-  )
-  if (req.method === 'OPTIONS') {
-    res.status(200).end()
-    return
-  }
-  return await fn(req, res)
+const corsOptions = {
+  origin: '*',
+  credentials:true,
+  optionsSuccessStatus: 200
 }
-
-const handler = (req, res) => {
-  const d = new Date()
-  res.end(d.toString())
-}
-app.use(allowCors(handler))
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/profile', profileRouter)
