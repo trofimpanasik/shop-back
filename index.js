caa4e0e94ae987a2e954798f8f8716dc082a6f5f
trofim <58886419+trofimpanasik@users.noreply.github.com>
2022-11-27 15:48:42 +0300

    Update index.js

--- index.js
+++ index.js
@@ -15,12 +15,30 @@ const server = httpServer.createServer(app)
 const PORT = process.env.PortServer
 
 //const whitelist = ["http://localhost:5173", "https://shop-ba9bb.web.app"]
-const corsOptions = {
-  origin: '*',
-  credentials:true,
-  optionsSuccessStatus: 200
-}
-app.use(cors(corsOptions))
+// const corsOptions = {
+//   origin: '*',
+//   credentials:true,
+//   optionsSuccessStatus: 200
+// }
+// app.use(cors(corsOptions))
+app.use(function (req, res, next) {
+
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'https://shop-ba9bb.web.app');
+
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 app.use(express.json())
 app.use('/api/auth', authRouter)
 app.use('/api/profile', profileRouter)
