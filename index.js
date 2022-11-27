46e4d6204434331c529562ca01441211a6a9d85c
trofim <58886419+trofimpanasik@users.noreply.github.com>
2022-11-27 16:40:55 +0300

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PortServer
 
 //const whitelist = ["http://localhost:5173", "https://shop-ba9bb.web.app"]
 const corsOptions = {
-  origin: 'https://shop-ba9bb.web.app/',
+  origin: '*',
   credentials:true,
   optionsSuccessStatus: 200
 }
