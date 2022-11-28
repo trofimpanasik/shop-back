104c9f74be69d1c08108d64b8201752d310fdb15
trofim <58886419+trofimpanasik@users.noreply.github.com>
2022-11-29 02:36:11 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,4 +1,4 @@
 import { MongoClient } from 'mongodb'
-import 'dotenv/config'
+// import 'dotenv/config'
 
-export const client = new MongoClient(process.env.dbURL, { useUnifiedTopology: true, useNewUrlParser: true, connectTimeoutMS: 30000, keepAlive: false })
+export const client = new MongoClient('mongodb+srv://turizm:g7446!BS9bu4vUf@shop.rq8wc8p.mongodb.net/?retryWrites=true&w=majority', { useUnifiedTopology: true, useNewUrlParser: true, connectTimeoutMS: 30000, keepAlive: false })
